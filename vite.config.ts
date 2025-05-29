5155869db58ecf34ac535a3067c3c245dff5cf3b
CecedeKP <99571735+Bamamou@users.noreply.github.com>
2025-05-29 20:54:04 +0800

    First deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Bamamou_Nicolas_Career/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Nicolas_Bamamou_Career_Chinese/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
