dc55c5158fbe35bed6117e4786ff144a80320d15
Nicolas Bamamou <niba5882@colorado.edu>
2025-06-02 21:08:19 +0800

    added a new route

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/Nicolas_Bamamou_Career' : '/'}>
+      <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/Nicolas_Bamamou_Career_Chinese/' : '/'}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/project/:id" element={<ProjectDetail />} />
