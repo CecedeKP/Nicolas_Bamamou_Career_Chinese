0b009e3968d8f869f3753c278df8aaa175dcb3c1
Nicolas Bamamou <niba5882@colorado.edu>
2025-06-02 20:49:48 +0800

    This to correct the chunk size and also the package.json file

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
-export default defineConfig({  plugins: [react()],
+export default defineConfig({
+  plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/Nicolas_Bamamou_Career_Chinese/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  assetsInclude: ['**/*.pdf']
+  assetsInclude: ['**/*.pdf'],
+  build: {
+    minify: 'terser',
+    chunkSizeWarningLimit: 1000, // Increase the limit to 1000kb
+    terserOptions: {
+      format: {
+        comments: false,
+      },
+    },
+    rollupOptions: {
+      output: {
+        format: 'iife'
+      }
+    }
+  }
 })
