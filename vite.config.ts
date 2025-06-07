76ea2c315da70ab017e660830d784814a7093528
NicolasNicolas <>
2025-06-07 19:48:50 +0800

    there we go

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,13 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Nicolas_Bamamou_Career_Chinese/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  assetsInclude: ['**/*.pdf'],
   build: {
-    minify: 'terser',
-    chunkSizeWarningLimit: 1000, // Increase the limit to 1000kb
-    terserOptions: {
-      format: {
-        comments: false,
-      },
-    },
-    rollupOptions: {
-      output: {
-        format: 'iife'
-      }
-    }
+    outDir: 'dist',
+    sourcemap: true
   }
 })
